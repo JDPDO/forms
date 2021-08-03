5b5b5541230dd473114c7cf7e6729b61f27281ff
Jan Petersen <11973633+JDPDO@users.noreply.github.com>
2021-08-03 09:08:44 +0200

    Improve run.sh: Add cleaning up on exit and automatic forms enabeling.

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # Start dev server on `localhost:8081`
+# Assuming executing user has access to a running docker daemon.
 
-npm run dev
-docker-compose up --build
+function onCancel {
+    docker-compose down -v --rmi local
+    docker-compose rm -sv 
+}
+
+trap onCancel EXIT
+
+NODE_ENV=development npm run dev
+# @see https://unix.stackexchange.com/questions/611675/run-a-command-in-parallel-and-wait-for-specific-output
+docker-compose up --build |
+    tee /dev/tty | {
+        cat > /dev/null
+        grep -q "apache2 -D FOREGROUND" && docker-compose exec -u www-data nextcloud php occ app:enable forms
+    }
